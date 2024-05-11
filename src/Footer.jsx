43c8dea1accb0533755af2ec5b9aa8be20ffd451
Jake Browning <jacobbrowning90@gmail.com>
2024-05-11 14:29:12 +0100

    Add placeholder Footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Created by Jake Browning for The Odin Project, 2024.</p>
+      <a href="http://jake-browning.com/" target="_blank">Portfolio</a>
+    </footer>
+  );
+}
+
+export default Footer;
