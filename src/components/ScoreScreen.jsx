a8aa7e945b5a3eb1c542b34581550739c20b80dd
Jake Browning <jacobbrowning90@gmail.com>
2024-05-12 15:36:00 +0100

    Add test array of users for scoreboard

--- src/components/ScoreScreen.jsx
+++ src/components/ScoreScreen.jsx
@@ -1,7 +1,22 @@
 function ScoreScreen({ returnHome }) {
+
+  // Test Array, change to fetch
+  const players = [
+    { name: "Ann", score: 12000 },
+    { name: "Ben", score: 11000 },
+    { name: "Cal", score: 10000 },
+  ];
+
   return (
     <div className="scoreScreen">
       <h1>Score Screen</h1>
+
+      <ol>
+      {players.map((player) => {
+        return <li key={player}>{player.name}: {player.score}</li>
+      })}
+      </ol>
+
       <button onClick={returnHome}>Return Home</button>
     </div>
   );
