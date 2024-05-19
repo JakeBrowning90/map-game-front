df6a4ae9c5a07826cd256e62d300af5edd7efa7a
Jake Browning <jacobbrowning90@gmail.com>
2024-05-19 17:03:55 +0100

    Start screen restyle

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -5,7 +5,12 @@ function StartScreen({ navToScoreboard, navToGame, drawTileSet }) {
   };
   return (
     <div className="startScreen">
-      <h1>Start Screen</h1>
+      <h1 className="startTitle">US Capitals</h1>
+      <ul className="startUl">
+        <li>Correctly identify all the cities marked on the map.</li>
+        <li>Click on a star to see a list of city names.</li>
+        <li>Think carefully: incorrect guesses are penalized!</li>
+      </ul>
       <button onClick={startGame}>Start Game</button>
       <button onClick={navToScoreboard}>High Scores</button>
     </div>
